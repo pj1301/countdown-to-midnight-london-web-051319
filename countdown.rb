19bd32e5f9fb608a61ad459a2134a60b97acbb08
James Hayward <james.hayward6@icloud.com>
2019-04-21 01:03:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown(num)
-  n = num
-  countdown_output = "<n> SECOND(S)"
-  until n < -1
-    n -= 1
+  i = num
+  countdown_output = "#{i} SECOND(S)"
+  until i < -1
+    i -= 1
     puts countdown_output
   end
   return "HAPPY NEW YEAR!"
