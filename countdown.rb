ab1c03efe79c0dc1fa59b9dd40e39da827587763
James Hayward <james.hayward6@icloud.com>
2019-04-21 01:37:48 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  i = num
+  # countdown_output = "#{i} SECOND(S)"
+  until i < 1 #appears to always need a < or > based expression. = doesn't work.
+    puts "#{i} SECOND(S)!" #despite stating an <n> rather than #{i} we can still use this expression and it registers as being correct with the test.
+    i -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(delay)
+  sleep(delay) #wow that was easy!!!
 end
