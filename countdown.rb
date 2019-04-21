b603271982a2e9920b234803edbcd50c1c95353a
James Hayward <james.hayward6@icloud.com>
2019-04-21 01:07:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown(num)
   i = num
   # countdown_output = "#{i} SECOND(S)"
   until i < 1 #appears to always need a < or > based expression. = doesn't work.
-    puts "#{i} SECOND(S)!"
+    puts "#{i} SECOND(S)!" #despite stating an <n> rather than #{i} we can still use this expression and it registers as being correct with the test.
     i -= 1
   end
   return "HAPPY NEW YEAR!"
