5d3ed5faa2468e66c632d0413597e48b42bb40fb
James Hayward <james.hayward6@icloud.com>
2019-04-21 01:04:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,8 @@
 def countdown(num)
   i = num
   # countdown_output = "#{i} SECOND(S)"
-  until i < -1
-    puts "#{i} SECOND(S)"
+  until i = 0
+    puts "#{i} SECOND(S)!"
     i -= 1
   end
   return "HAPPY NEW YEAR!"
